38d381ca63c53d94e1c8ceaac5b4e5ffd6f527a4
farhan <shaikfarhanhaque@gmail.com>
2025-04-23 18:37:46 +0530

    All files are added

---
+++ src/ex_23042025/Farhan_QA.java
@@ -0,0 +1,7 @@
+package ex_23042025;
+
+public class Farhan_QA {
+    public static void main(String[] args) {
+        System.out.println("You can give the file name with underscore too _");
+    }
+}
