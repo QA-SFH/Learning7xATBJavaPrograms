a8e26de96c894006a9ae1b790b73ed6b21289de3
farhan <shaikfarhanhaque@gmail.com>
2025-04-23 18:34:48 +0530

    All files are added

---
+++ src/ex_23042025/Lab013.java
@@ -0,0 +1,10 @@
+package ex_23042025;
+
+public class Lab013 {
+    public static void main(String[] args) {
+
+    }
+    //public static void main(String[] args) { // Not the entry point
+
+    //}
+}
