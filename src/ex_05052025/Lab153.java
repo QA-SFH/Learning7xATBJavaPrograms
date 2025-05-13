fe61628e4a4ce759d288e40e94f872f3b5d63137
farhan <shaikfarhanhaque@gmail.com>
2025-05-13 18:22:43 +0530

    All files are added

--- src/ex_05052025/Lab153.java
+++ src/ex_05052025/Lab153.java
@@ -2,14 +2,26 @@ package ex_05052025;
 
 public class Lab153 {
     public static void main(String[] args) {
-            BuildingBP dlf = new BuildingBP();
+
+        //  Class - Attribute / properties - data Types - primitve or non primitive.
+        // Class - Behaviour /Methods - functions -
+
+        // object - real entity - class -
+        // ClassName classRef = new ClassName();
+
+        // new ClassName(); - Object
+        // classRef - Class Ref
+        // ClassName = Class Name which blueprint you are using
+
+
+            BuildingBP dlfref = new BuildingBP();
             dlf.name = "DLF Pvt builder";
             dlf.noOfRooms = 100;
 
             dlf.uselift();
 
 
-        BuildingBP prestige = new BuildingBP();
+        BuildingBP prestigeref = new BuildingBP();
         prestige.name = "Prestige Pvt builder";
         prestige.noOfRooms = 100;
 
